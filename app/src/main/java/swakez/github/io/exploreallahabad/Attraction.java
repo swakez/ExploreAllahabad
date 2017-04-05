0add967c5d073a419df800a2a0f1d981289c13eb
swati-93 <swati.1993cs@gmail.com>
2017-04-06 00:01:34 +0530

    initial commit

---
+++ app/src/main/java/swakez/github/io/exploreallahabad/Attraction.java
@@ -0,0 +1,21 @@
+package swakez.github.io.exploreallahabad;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by swati on 05/04/2017.
+ */
+
+public class Attraction extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.default_layout,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/swakez/github/io/exploreallahabad/MainActivity.java
@@ -0,0 +1,23 @@
+package swakez.github.io.exploreallahabad;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout_category);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
